47b7ace55112d5e340eb3f16299b665f2cf47e5d
Mateus-Bauerr <127158016+Mateus-Bauerr@users.noreply.github.com>
2023-05-15 20:36:20 -0300

    bd funcionou

--- index/cadastros/cadastro cao/cadastro.php
+++ index/cadastros/cadastro cao/cadastro.php
@@ -1,15 +1,28 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'ienh', 'mydb');
-if (!$conn) {
-    die('Could not connect: ');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $conn = mysqli_connect('localhost', 'root', 'ienh', 'mydb');
+    if (!$conn) {
+        die('Could not connect: ' . mysqli_connect_error());
+    }
+
+    $CPF = $_POST["CPF"];
+    $Nome = $_POST["Nome"];
+    $Data_Nascimento = $_POST["Data_Nascimento"];
+    $Telefone = $_POST["Telefone"];
+    $Endereco = $_POST["Endereco"];
+    $Email = $_POST["Email"];
+    $senha = $_POST["senha"];
+    $sobrenome = $_POST["sobrenome"];
+
+    $sql = "INSERT INTO users (CPF, Nome, Data_Nascimento, Telefone, Endereco, Email, senha, sobrenome) 
+            VALUES ('$CPF', '$Nome', '$Data_Nascimento', '$Telefone', '$Endereco', '$Email', '$senha', '$sobrenome')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Cadastro realizado com sucesso";
+    } else {
+        echo "Erro ao cadastrar: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 }
-
-$sql = 'INSERT INTO users(CPF, Nome, Data_Nascimento, Telefone, Endereco, Email, senha, sobrenome) values("' . $_POST["CPF"] . '","' . $_POST["Nome"] . '" , "'. $_POST["Data_Nascimento"] . '","' . $_POST["Telefone"] . '" . "' . $_POST["Endereco"] . '" . "' . $_POST["Email"] . '" . "' . $_POST["senha"] . '" . "' . $_POST["sobrenome"] . '")';
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
 ?>
