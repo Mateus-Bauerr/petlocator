49863c9f8589cfe5448762c18c0213fce90f9ce9
Mateus-Bauerr <127158016+Mateus-Bauerr@users.noreply.github.com>
2023-05-15 19:23:52 -0300

    banco de dados

--- index/cadastros/cadastro cao/cadastro.php
+++ index/cadastros/cadastro cao/cadastro.php
@@ -1,10 +1,10 @@
 <?php
-$conn = mysqli_connect('localhost', 'laboratorio', 'estudante', 'projeto_integrador');
+$conn = mysqli_connect('localhost', 'root', 'ienh', 'petlocator');
 if (!$conn) {
     die('Could not connect: ');
 }
 
-$sql = 'INSERT INTO usuarios(usuario, senha) values("' . $_POST["usuario"] . '","' . $_POST["senha"] . '")';
+$sql = 'INSERT INTO users(CPF, Nome, Data_Nascimento, Telefone, Endereco, Email, senha, sobrenome) values("' . $_POST["CPF"] . '","' . $_POST["Nome"] . '" , "'. $_POST["Data_Nascimento"] . '","' . $_POST["Telefone"] . '" . "' . $_POST["Endereco"] . '" . "' . $_POST["Email"] . '" . "' . $_POST["senha"] . '" . "' . $_POST["sobrenome"] . '")';
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
