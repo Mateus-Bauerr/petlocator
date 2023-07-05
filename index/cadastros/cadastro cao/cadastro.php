b02bdfc24c13c2c8afd2ace9bc62dd7d914dbf89
Mateus-Bauerr <127158016+Mateus-Bauerr@users.noreply.github.com>
2023-07-04 22:43:47 -0300

    BANCO DE DADOS XAMPP ESTÁ FUNCIONANDO NO MEU PC

--- index/cadastros/cadastro cao/cadastro.php
+++ index/cadastros/cadastro cao/cadastro.php
@@ -1,28 +1,30 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $conn = mysqli_connect('localhost', 'root', 'ienh', 'mydb');
-    if (!$conn) {
-        die('Could not connect: ' . mysqli_connect_error());
-    }
-
-    $CPF = $_POST["CPF"];
-    $Nome = $_POST["Nome"];
-    $Data_Nascimento = $_POST["Data_Nascimento"];
-    $Telefone = $_POST["Telefone"];
-    $Endereco = $_POST["Endereco"];
-    $Email = $_POST["Email"];
-    $senha = $_POST["senha"];
-    $sobrenome = $_POST["sobrenome"];
-
-    $sql = "INSERT INTO users (CPF, Nome, Data_Nascimento, Telefone, Endereco, Email, senha, sobrenome) 
-            VALUES ('$CPF', '$Nome', '$Data_Nascimento', '$Telefone', '$Endereco', '$Email', '$senha', '$sobrenome')";
-
-    if (mysqli_query($conn, $sql)) {
-        echo "Cadastro realizado com sucesso";
-    } else {
-        echo "Erro ao cadastrar: " . mysqli_error($conn);
-    }
-
-    mysqli_close($conn);
+$conn = mysqli_connect('localhost', 'root', '', 'mydb');
+if (!$conn) {
+    die('Could not connect: ' . mysqli_connect_error());
 }
+
+$CPF = isset($_POST['CPF']) ? $_POST['CPF'] : '';
+$Nome = isset($_POST['Nome']) ? $_POST['Nome'] : '';
+$dataNascimento = isset($_POST['Data_Nascimento']) ? $_POST['Data_Nascimento'] : '';
+$telefone = isset($_POST['Telefone']) ? $_POST['Telefone'] : '';
+$endereco = isset($_POST['Endereco']) ? $_POST['Endereco'] : '';
+$email = isset($_POST['Email']) ? $_POST['Email'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+$sobrenome = isset($_POST['sobrenome']) ? $_POST['sobrenome'] : '';
+
+// Verifica se todos os campos obrigatórios foram preenchidos
+if ($CPF !== '' && $Nome !== '' && $dataNascimento !== '' && $telefone !== '' && $endereco !== '' && $email !== '' && $senha !== '' && $sobrenome !== '') {
+    $sql = "INSERT INTO users (CPF, Nome, Data_Nascimento, Telefone, Endereco, Email, senha, sobrenome) VALUES ('$CPF', '$Nome', '$dataNascimento', '$telefone', '$endereco', '$email', '$senha', '$sobrenome')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+} else {
+    echo "Please fill all the required fields.";
+}
+
+$conn->close();
 ?>
